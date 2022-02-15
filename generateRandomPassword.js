cd18c66b0420cc880eb1e527bff0f5a53917f055
Urdo <karol.ugarenko@gmail.com>
2022-02-15 20:50:00 +0100

    try make better password generate

--- generateRandomPassword.js
+++ generateRandomPassword.js
@@ -5,15 +5,21 @@ let newPassword = "a";
 
 const generateNewPassowrd = () => {
     console.log("work");  // check function work
-    const indexRoll1 = (Math.floor(Math.random() * password.length));
-    const indexRoll2 = (Math.floor(Math.random() * password.length));
-    const indexRoll3 = (Math.floor(Math.random() * password.length));
-    const indexRoll4 = (Math.floor(Math.random() * password.length));
-    const indexRoll5 = (Math.floor(Math.random() * password.length));
-    const indexRoll6 = (Math.floor(Math.random() * password.length));
 
-    document.querySelector("section").textContent = password[indexRoll1] + password[indexRoll2]
-    + password[indexRoll3] + password[indexRoll4]+ password[indexRoll5]+ password[indexRoll6]; 
+// stupid idea
+
+    // const indexRoll1 = (Math.floor(Math.random() * password.length));
+    // const indexRoll2 = (Math.floor(Math.random() * password.length));
+    // const indexRoll3 = (Math.floor(Math.random() * password.length));
+    // const indexRoll4 = (Math.floor(Math.random() * password.length));
+    // const indexRoll5 = (Math.floor(Math.random() * password.length));
+    // const indexRoll6 = (Math.floor(Math.random() * password.length));
+
+    // document.querySelector("section").textContent = password[indexRoll1] + password[indexRoll2]
+    // + password[indexRoll3] + password[indexRoll4]+ password[indexRoll5]+ password[indexRoll6]; 
+
+
+
 } ;
 
 btn.addEventListener("click", generateNewPassowrd);
