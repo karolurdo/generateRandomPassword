efb7063824b39332192197b8147e0449c1e23a3b
Urdo <karol.ugarenko@gmail.com>
2022-02-15 20:42:07 +0100

    take each letters all time till end

--- generateRandomPassword.js
+++ generateRandomPassword.js
@@ -1,7 +1,10 @@
 const btn = document.querySelector('button');
+const password = ["a", "b", "c", "d", "e", "f", "g"];
 
-const generateNewPassowrd = function() {
+const generateNewPassowrd = () => {
     console.log("work");  // check function work
-} 
+    const indexRoll = (Math.floor(Math.random() * password.length));
+    document.querySelector("section").textContent = password[indexRoll] 
+} ;
 
-btn.addEventListener("click", generateNewPassowrd)
+btn.addEventListener("click", generateNewPassowrd);
