f02819fb27fde4c195e7ccd89dd1f7d80dc62bf8
Urdo <karol.ugarenko@gmail.com>
2022-02-15 20:47:27 +0100

    stupid idea but work :)

--- generateRandomPassword.js
+++ generateRandomPassword.js
@@ -1,10 +1,19 @@
 const btn = document.querySelector('button');
 const password = ["a", "b", "c", "d", "e", "f", "g"];
+const length = 8;
+let newPassword = "a";
 
 const generateNewPassowrd = () => {
     console.log("work");  // check function work
-    const indexRoll = (Math.floor(Math.random() * password.length));
-    document.querySelector("section").textContent = password[indexRoll] 
+    const indexRoll1 = (Math.floor(Math.random() * password.length));
+    const indexRoll2 = (Math.floor(Math.random() * password.length));
+    const indexRoll3 = (Math.floor(Math.random() * password.length));
+    const indexRoll4 = (Math.floor(Math.random() * password.length));
+    const indexRoll5 = (Math.floor(Math.random() * password.length));
+    const indexRoll6 = (Math.floor(Math.random() * password.length));
+
+    document.querySelector("section").textContent = password[indexRoll1] + password[indexRoll2]
+    + password[indexRoll3] + password[indexRoll4]+ password[indexRoll5]+ password[indexRoll6]; 
 } ;
 
 btn.addEventListener("click", generateNewPassowrd);
